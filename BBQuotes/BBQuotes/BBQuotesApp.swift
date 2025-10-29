7a1dba808a214853021d0fdd3911d72eba257e7d
Allana Idalgo <allana.idalgo@wildlifestudios.com>
2025-10-29 03:02:52 -0300

    initial commit

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Allana Idalgo on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes/BBQuotes/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BBQuotes
+//
+//  Created by Allana Idalgo on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Tab("Breaking Bad", systemImage: "tortoise") {
+                Text("Breaking Bad")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+            
+            Tab("Better Call Saul", systemImage: "briefcase") {
+                Text("Better Call Saul")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
